0e62d68db9acd73143739f4111a97c308be3724d
zac-freeman <zpfreeman14@gmail.com>
2019-07-14 02:24:30 -0500

    fixed loading scripts contained in html retrieved by loadHTML()
    scripts are run using eval() and retrieved asynchronously using unique xhr objects

--- src/include-html.js
+++ src/include-html.js
@@ -1,3 +1,9 @@
+// loadHTML(): For each <div> containing an attribute of the form `include-html = "path/to/content"`,
+//             set innerHTML of said <div> with content at path,
+//             and execute javascript contained within <script> tags.
+//
+// TODO: run scripts contained within new innerHTML content when it is loaded
+//       filter out all comments and debug statements from code before it is loaded, to optimize download times
 function loadHTML() {
 	let elements = document.getElementsByTagName("div");
 
@@ -6,17 +12,47 @@ function loadHTML() {
 
 		if (source) {
 			// Make an HTTP request using the attribute value as the file name
-			let xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
+			let xhr = new XMLHttpRequest();
+
+			xhr.onreadystatechange = function() {
 				if (this.readyState == 4) {
-					if (this.status == 200) {element.innerHTML = this.responseText;}
-					if (this.status == 404) {element.innerHTML = "Page not found.";}
+					if (this.status == 200) {
+						element.innerHTML = this.responseText;
+
+						loadScripts([...element.getElementsByTagName("script")]);
+					}
+					if (this.status == 404) {
+						element.innerHTML = "Not found.";
+					}
 				}
 			}
-			xhttp.open("GET", source, true);
-			xhttp.send();
+			xhr.open("GET", source, true);
+			xhr.send();
 		}
 	}
+}
+
+function loadScripts(scripts) {
+	scripts.forEach(script => {
+		loadScript(script);
+	})
+}
+
+// loadScript(HTMLScriptElement script): For each script, create a new XMLHttpRequest and eval the response.
+//                                       Async calls to the same xhr object show unexpected behavior.
+function loadScript(script) {
+	let xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				eval(this.responseText)
+			}
+		}
+	}
+
+	xhr.open("GET", script.src, true);
+	xhr.send();
 }
 
 function unloadHTML() {
